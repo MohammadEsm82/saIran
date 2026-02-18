61dce62ae8432c7df9a41b10132f180dd328b32d
MohammadEsm82 <madesm82@gmail.com>
2026-02-18 20:15:27 +0330

    +footer, css variables

--- src/layout/NavFooter/NavFooter.jsx
+++ src/layout/NavFooter/NavFooter.jsx
@@ -1,5 +1,5 @@
 import { Link, Outlet } from "react-router";
-import styles from "./NavFooter.module.css";
+import styles  from "./NavFooter.module.css";
 
 import logo from "../../assets/images/logo.png";
 
@@ -27,7 +27,37 @@ const NavFooter = () => {
       </div>
 
       <footer className={styles.footer}>
-        <p>درباره ما</p>
+        <div className={styles.footercontent}>
+
+
+          <div className={styles.left}>
+            <h1 className={styles.footertitle}>ارتباط با ما</h1>
+            <p className={styles.footertext}>اصفهان-حکیم نظامی-جنب بانک ملت</p>
+            <p className={styles.footertext}> تلفن همراه: 091370097</p>
+            <p className={styles.footertext}>تلفکس : 7648376</p>
+            <p className={styles.footertext}>moslehINFO@gmail.com</p>
+
+          </div>
+
+
+          <div className={styles.right}>
+                <h3 className={styles.footertext}>صنعت تجهیزات پزشکی صاایران</h3>
+                <p className={styles.footertext}>فعالیت خود را را در زمینه ای طراحی و تولید تجهیزات پزشکی
+                 اغاز کرده
+                </p>
+
+          </div>
+          
+         
+
+        </div>
+
+        <div className={styles.copyright}>
+          <p > تمام حقوق نزد گروه صنعت تجهیزات پزشکی ما محفوظ است.</p>
+
+        </div>
+        
+
       </footer>
     </div>
   );
