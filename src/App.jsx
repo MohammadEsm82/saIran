925d387a38adcf864a055b2d93ed51559c6d6aa8
MohammadEsm82 <madesm82@gmail.com>
2026-02-26 19:21:24 +0330

    +ourstory

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import styles from "./App.module.css";
+
 import Carousel from "./components/Carousel/Carousel";
+import ProductCarousel from "./components/ProductCarousel/ProductCarousel";
+import ParallaxBG from "./components/ParallaxBG/ParallaxBG";
 
 import image1 from "./assets/images/1.webp";
 import image2 from "./assets/images/2.jpg";
 import image3 from "./assets/images/3.webp";
-import ProductCarousel from "./components/ProductCarousel/ProductCarousel";
-import ParallaxBG from "./components/ParallaxBG/ParallaxBG";
+import ourstoryImage from "./assets/images/ourstory.jpg"
+
+import { FaPlay } from "react-icons/fa";
 
 function App() {
 
@@ -30,9 +34,35 @@ function App() {
           }
         ]}/>
 
-        <ProductCarousel />
+      <ProductCarousel />
 
-        <ParallaxBG />
+      <ParallaxBG />
+
+      <div className={styles.ourstory}>
+        <div className={styles.details}>
+          <h3>داستان ما | Our Story</h3>
+          <p>
+            صنعت تجهیزات پزشکی صاایران فعالیت خود را در زمینه‌ی طراحی و تولید تجهیزات پزشکی و آزمایشگاهی پیشرفته از سال 1378 آغاز کرده است.
+            <br/>
+            در حال حاضر محصولات پزشکی متعددی توسط محققان و دانش پژوهان جوان این صنعت طراحی و تولید شده است که علاوه بر تأمین نیازمندی مراکز درمانی داخل کشور، به سایر کشورها نیز صادر می گردد.
+            <br/>
+            طراحی و تولید محصولات جدید و متنوع، همگام با تکنولوژی روز، همواره از راهبرد های اصلی ما بوده است...
+          </p>
+          <a href="#">درباره ما</a>
+                 
+        </div>
+
+
+
+        <div>
+          <a href="https://www.aparat.com/v/ddg406e" target="_blank" className={styles.playBtn}>
+            <span>
+              <FaPlay/>
+            </span>
+          </a>
+          <img className={styles.ourstoryimage} src={ourstoryImage} alt=""/>
+        </div>
+      </div>
     </div>
   )
 }
